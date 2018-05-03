7d3fc797bce455eedbf56c96eedcf12feeea5169
Avery Wagar <ajmw.subs@gmail.com>
2018-05-02 19:43:20 -0700

    Looks good

--- POLYBAR/run_polybar.sh
+++ POLYBAR/run_polybar.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+##!/bin/sh
 
-killall polybar
-polybar traybar -c ~/.polybar &
-polybar topbar1 -c ~/.polybar &
-polybar topbar2 -c ~/.polybar &
+#killall polybar
+#polybar traybar -c ~/.polybar &
+#polybar topbar1 -c ~/.polybar &
+#polybar topbar2 -c ~/.polybar &
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar base &
+  polybar --reload base -c ~/.polybar  &
+  polybar -c ~/.polybar --reload traybar &
+
