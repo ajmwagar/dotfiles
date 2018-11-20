5392de1a4ad4b8e9c8464134fe9155d7966d7928
Avery Wagar <ajmw.subs@gmail.com>
2018-11-19 19:30:07 -0800

    runpoly

---
+++ run_polybar.sh
@@ -0,0 +1,20 @@
+##!/bin/sh
+
+#killall polybar
+#polybar traybar -c ~/.polybar &
+#polybar topbar1 -c ~/.polybar &
+#polybar topbar2 -c ~/.polybar &
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar base &
+  polybar --reload topbar1 -c ~/.polybar  &
+  polybar --reload topbar2 -c ~/.polybar  &
+  polybar -c ~/.polybar --reload traybar &
+
